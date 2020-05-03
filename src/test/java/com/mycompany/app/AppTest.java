ce8178d857e7f8431da49baf6692a950dfaf1045
Osman Burucu - IBM <osman.burucu@at.ibm.com>
2020-05-03 16:25:16 +0200

    aa

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello from my World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello from My World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello from my World!\"");
+            fail("\"message\" is not \"Hello from My World!\"");
         }
     }
 
