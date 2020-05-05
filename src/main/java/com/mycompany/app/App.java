091b1a3d7d7503464f5a3667d8ec69e79c58e036
Osman Burucu - IBM <osman.burucu@at.ibm.com>
2020-05-05 11:42:02 +0200

    JDEMO-3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello from my World!";
+    private final String message = "Hello TO my World!";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello from my World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello TO my World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello from my World!\"");
+            fail("\"message\" is not \"Hello TO my World!\"");
         }
     }
 
